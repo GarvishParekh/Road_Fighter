89bebb7fe6e24daecc108f92e5e2a3d76edf62c5
Garvish Parekh <garvish.p@theintellify.com>
2024-05-15 19:07:40 +0530

    Bug fixes and photoshoot

--- Assets/_My Assets/Code/Behivour/EnableSelectedCar.cs
+++ Assets/_My Assets/Code/Behivour/EnableSelectedCar.cs
@@ -5,15 +5,32 @@ public class EnableSelectedCar : MonoBehaviour
 {
     [SerializeField] private CarStoreData carStoreData;
     [SerializeField] private List<Cars> carsList = new List<Cars>();
+    [SerializeField] private GameObject carHolder;
 
     public bool foundEquippedCar = false;
 
     private void Start()
     {
         //PlayerPrefs.GetInt(ConstantKeys.car)
+        if (carHolder != null)
+        {
+            carHolder.transform.localScale = Vector3.zero;
+            Invoke(nameof(ChangeCarDelay), 2f);
+        }
+        else
+        {
+            CarsClass carClass = carStoreData.equippedCarData.equippedCarclass;
+            int carIndex = carStoreData.equippedCarData.equippedCarIndex;
+            ChangeCar(carClass, carIndex);
+        }
+    }
+
+    private void ChangeCarDelay()
+    {
+        LeanTween.scale(carHolder, Vector3.one, 0.5f).setEaseInOutBounce();
+
         CarsClass carClass = carStoreData.equippedCarData.equippedCarclass;
         int carIndex = carStoreData.equippedCarData.equippedCarIndex;
-
         ChangeCar(carClass, carIndex);
     }
 
