5df3f40baee355061745d1bd50007295ef9a0f90
Garvish Parekh <garvish.p@theintellify.com>
2024-05-22 15:02:52 +0530

    Exposing swipe controls

---
+++ Assets/_My Assets/Code/Behivour/SwipeControlValues.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SwipeControlValues : MonoBehaviour
+{
+    public float controlSmoothness = 20f;
+    public float deltaThreshold = 0.2f;
+}
--- Assets/_My Assets/Code/Behivour/SwipeControls.cs
+++ Assets/_My Assets/Code/Behivour/SwipeControls.cs
@@ -1,35 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(SwipeControlValues))]
 public class SwipeControls : EventTrigger
 {
+    SwipeControlValues settings;
     public static SwipeControls instance;
 
     float direction;
+    float eventDelta;
+    float touchInfo;
 
     private void Awake()
-    {
-        instance = this;
-    }
+        => instance = this;
+
+    private void Start()
+        => settings = GetComponent<SwipeControlValues>();  
 
     public override void OnDrag(PointerEventData eventData)
     {
-        float eventDelta = eventData.delta.x / 10;
-        float touchInfo = Mathf.Abs(eventDelta);
-        if (touchInfo > 0.8f)
-        {
+        eventDelta = eventData.delta.x / settings.controlSmoothness;
+        touchInfo = Mathf.Abs(eventDelta);
+
+        if (touchInfo > settings.deltaThreshold)
             direction = eventDelta ;
-        }
         else
-        {
             direction = 0;
-        }
     }
 
     public override void OnEndDrag(PointerEventData eventData)
-    {
-        direction = 0; 
-    }
+        => direction = 0; 
 
     public float GetDirection()
     {
